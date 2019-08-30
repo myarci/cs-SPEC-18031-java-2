59a49044841765ca6217225c4513a6ba4623ef55
Metin Yarci <metin.yarci@aurea.com>
2019-08-30 09:22:36 +0300

    commit1

---
+++ directory1/directory2/ALongClass.java
@@ -0,0 +1,15 @@
+package directory1.directory2;
+
+public class ALongClass {
+    public void aLongMethod() {
+        int abc = 10;
+        int cde = abc * 5;
+        abc = cde + 7;
+    }
+
+    public void z (){
+        int ab = 10;
+        int cd = ab * 5;
+        ab = cd + 7;
+    }
+}
---
+++ directory3/directory4/AParentClass.java
@@ -0,0 +1,17 @@
+package directory3.directory4;
+
+public class AParentClass {
+
+    public void myParentMethod() {
+        int myVar = 0;
+    }
+
+    public class ALongClass {
+
+        public void myChildMethod() {
+            int myVar = 1;
+        }
+
+    }
+
+}
